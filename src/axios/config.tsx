b943a33a5cdc02817079978b0f147300d71366dd
fede8932 <federi.angel@gmail.com>
2024-09-03 18:10:31 -0300

    a

--- src/axios/config.tsx
+++ src/axios/config.tsx
@@ -6,11 +6,17 @@ interface AxiosInstances {
 
 const { VITE_MY_URL_BACKEND } = import.meta.env;
 
+// Obtener el subdominio del frontend
+const subdomain = window.location.hostname.split(".")[0];
+
 // eslint-disable-next-line prefer-const
 let axiosInstances: AxiosInstances = {
   api: axios.create({
     baseURL: VITE_MY_URL_BACKEND,
     withCredentials: true,
+    headers: {
+      "X-Subdomain": subdomain, // Añadir el subdominio como un header personalizado
+    },
   }),
 };
 
