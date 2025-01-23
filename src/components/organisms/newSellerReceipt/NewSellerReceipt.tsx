d3b298dfaf40b136f33726e74fc3144c224dd444
fede8932 <federi.angel@gmail.com>
2025-01-23 11:41:18 -0300

    act

--- src/components/organisms/newSellerReceipt/NewSellerReceipt.tsx
+++ src/components/organisms/newSellerReceipt/NewSellerReceipt.tsx
@@ -5,27 +5,43 @@ import styles from "./newSellerReceipt.module.css";
 // import { useSelector } from "react-redux";
 // import { RootState } from "../../../redux/store";
 import { DatePicker } from "antd";
-import { Button } from "semantic-ui-react";
+import { Button, Checkbox } from "semantic-ui-react";
 
 function NewSellerReceipt(): ReactNode {
+  type CheckType = { [key in "efec" | "cheq" | "tran"]: boolean };
+  type CheQueType = {
+    montoCh: string;
+    bancoCh: string;
+    numCh: string;
+    cobroCh: string | string[];
+  };
+  const [checkType, setCheckType] = useState<CheckType>({
+    efec: true,
+    cheq: false,
+    tran: false,
+  });
+
   const [montoEf, setMontoEf] = useState(0);
-  const [montoCh, setMontoCh] = useState(0);
   const [montoTr, setMontoTr] = useState(0);
 
   const [bancoTr, setBancoTr] = useState("");
-  const [bancoCh, setBancoCh] = useState("");
 
   const [op, setOp] = useState("");
-  const [numCh, setNumCh] = useState("");
 
   const [coment, setComent] = useState("");
-  const [cobroCh, setCobroCh] = useState<string | string[]>("");
+
+  const [chequeState, setChequeState] = useState<CheQueType[]>([
+    { montoCh: "", bancoCh: "", numCh: "", cobroCh: "" },
+  ]);
 
   const handleChange = (e: any, fn: any) => {
     fn(e.target.value);
   };
-
-  console.log(cobroCh);
+  const handleChequeChange = (e: any, i: number) => {
+    const newChequeState = [...chequeState];
+    newChequeState[i] = e.target.value;
+    setChequeState(newChequeState);
+  };
   //   const acountStatus: IAcountState = useSelector(
   //     (state: RootState) => state.acount
   //   );
@@ -35,86 +51,154 @@ function NewSellerReceipt(): ReactNode {
     e.preventDefault();
   };
 
+  const handleChecked = (name: "efec" | "cheq" | "tran") => {
+    const newCheckType = { ...checkType };
+    newCheckType[name] = !newCheckType[name];
+    let update: boolean = false;
+    for (const key in newCheckType) {
+      if (newCheckType[key as keyof typeof newCheckType]) {
+        update = true;
+      }
+    }
+    if (update) {
+      setCheckType(newCheckType);
+    }
+  };
+
+  const addNewCheq = () => {
+    const newChequeState = [...chequeState];
+    newChequeState.push({ montoCh: "", bancoCh: "", numCh: "", cobroCh: "" });
+    setChequeState(newChequeState);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <span>Total facturado:</span>
         <span>${1111}</span>
       </div>
+      <div className={styles.cheCont}>
+        <Checkbox
+          label="Efectivo"
+          checked={checkType.efec}
+          onChange={() => handleChecked("efec")}
+        />
+        <Checkbox
+          label="Transferencia"
+          checked={checkType.tran}
+          onChange={() => handleChecked("tran")}
+        />
+        <Checkbox
+          label="Cheque"
+          checked={checkType.cheq}
+          onChange={() => handleChecked("cheq")}
+        />
+      </div>
       <form className={styles.formu} onSubmit={handleSubmit}>
-        <div className={styles.inputCont}>
-          <label>Monto en efectivo</label>
-          <input
-            className={styles.newImput}
-            placeholder="Monto en pesos en efectivo"
-            value={montoEf}
-            onChange={(e) => handleChange(e, setMontoEf)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Monto transferido</label>
-          <input
-            className={styles.newImput}
-            placeholder="Monto pagado por transferencia"
-            value={montoTr}
-            onChange={(e) => handleChange(e, setMontoTr)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Entidad financiera</label>
-          <input
-            className={styles.newImput}
-            placeholder="Ingresar el nombre de la entidad"
-            value={bancoTr}
-            onChange={(e) => handleChange(e, setBancoTr)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Número de operación</label>
-          <input
-            className={styles.newImput}
-            placeholder="Ingresar el N° de operación emitido por la entidad"
-            value={op}
-            onChange={(e) => handleChange(e, setOp)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Monto de cheque</label>
-          <input
-            className={styles.newImput}
-            placeholder="Monto pagado por cheque"
-            value={montoCh}
-            onChange={(e) => handleChange(e, setMontoCh)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Entidad bancaria</label>
-          <input
-            className={styles.newImput}
-            placeholder="Nombre de la entidad que figura en el cheque"
-            value={bancoCh}
-            onChange={(e) => handleChange(e, setBancoCh)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Número de cheque</label>
-          <input
-            className={styles.newImput}
-            placeholder="Ingresar tal cual figura en el cheque"
-            value={numCh}
-            onChange={(e) => handleChange(e, setNumCh)}
-          />
-        </div>
-        <div className={styles.inputCont}>
-          <label>Fecha de cobro</label>
-          <DatePicker
-            placeholder="Seleccioná la fecha"
-            onChange={(_, ds) => {
-              setCobroCh(ds);
-            }}
-            className={styles.newImput}
-          />
-        </div>
+        {checkType.efec ? (
+          <div className={styles.inputCont}>
+            <label>Monto en efectivo</label>
+            <input
+              className={styles.newImput}
+              placeholder="Monto en pesos en efectivo"
+              value={montoEf}
+              onChange={(e) => handleChange(e, setMontoEf)}
+            />
+          </div>
+        ) : null}
+        {checkType.tran ? (
+          <>
+            <div className={styles.inputCont}>
+              <label>Monto transferido</label>
+              <input
+                className={styles.newImput}
+                placeholder="Monto pagado por transferencia"
+                value={montoTr}
+                onChange={(e) => handleChange(e, setMontoTr)}
+              />
+            </div>
+            <div className={styles.inputCont}>
+              <label>Entidad financiera</label>
+              <input
+                className={styles.newImput}
+                placeholder="Ingresar el nombre de la entidad"
+                value={bancoTr}
+                onChange={(e) => handleChange(e, setBancoTr)}
+              />
+            </div>
+            <div className={styles.inputCont}>
+              <label>Número de operación</label>
+              <input
+                className={styles.newImput}
+                placeholder="Ingresar el N° de operación emitido por la entidad"
+                value={op}
+                onChange={(e) => handleChange(e, setOp)}
+              />
+            </div>
+          </>
+        ) : null}
+        {checkType.cheq
+          ? chequeState.map((item, i) => (
+              <>
+                <div className={styles.inputCont}>
+                  <label>{chequeState.length < 2 ? "Monto de cheque" : `Monto de cheque ${i+1}`}</label>
+                  <input
+                    className={styles.newImput}
+                    placeholder="Monto pagado por cheque"
+                    value={item.montoCh}
+                    onChange={(e) => handleChequeChange(e, i)}
+                  />
+                </div>
+                <div className={styles.inputCont}>
+                  <label>{chequeState.length < 2 ? "Entidad bancaria" : `Entidad bancaria de cheque ${i+1}`}</label>
+                  <input
+                    className={styles.newImput}
+                    placeholder="Nombre de la entidad que figura en el cheque"
+                    value={item.bancoCh}
+                    onChange={(e) => handleChequeChange(e, i)}
+                  />
+                </div>
+                <div className={styles.inputCont}>
+                  <label>{chequeState.length < 2 ? "Número de cheque" : `Número de cheque ${i+1}`}</label>
+                  <input
+                    className={styles.newImput}
+                    placeholder="Ingresar tal cual figura en el cheque"
+                    value={item.numCh}
+                    onChange={(e) => handleChequeChange(e, i)}
+                  />
+                </div>
+                <div className={styles.inputCont}>
+                <label>{chequeState.length < 2 ? "Fecha de cobro" : `Fecha de cobro de cheque ${i+1}`}</label>
+                  <DatePicker
+                    placeholder="Seleccioná la fecha"
+                    onChange={(_, ds) => {
+                      const newChequeState = [...chequeState];
+                      newChequeState[i].cobroCh = ds;
+                      setChequeState(newChequeState);
+                    }}
+                    className={styles.newImput}
+                  />
+                </div>
+                {chequeState.length < 2 ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "end",
+                      width: "95%",
+                    }}
+                  >
+                    <button
+                      type="button"
+                      onClick={addNewCheq}
+                      className={styles.addBut}
+                    >
+                      Add
+                    </button>
+                  </div>
+                ) : null}
+              </>
+            ))
+          : null}
         <div className={styles.inputCont}>
           <label>Comentarios</label>
           <textarea
