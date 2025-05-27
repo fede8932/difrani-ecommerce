5ada612966c7fa339453c898963842cf9df33d6d
fede8932 <federi.angel@gmail.com>
2025-05-27 08:31:29 -0300

    act

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+        name: "Ecommerce Difrani",
+        short_name: "DifraniShop",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./icon/icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./icon/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+    }),],
   server: {
     port: 5176, // Cambia este número al puerto que prefieras
     host: "0.0.0.0",
